a870c6c98c272b473680742d8f89067c802aa466
xzk-laptop <xzk09014134@outlook.com>
2018-09-29 19:20:51 +0800

    add stat_by_year

---
+++ stat_by_year.py
@@ -0,0 +1,38 @@
+from datetime import date
+from multiprocessing import Pool
+import daily_page_parse
+import my_logger
+import result_write
+
+
+logger = my_logger.get_logger('stat.log')
+
+
+def stat_by_month(begin_day, end_day):
+    day = begin_day
+    pool = Pool()
+    result_list = list()
+    for d in range((end_day-begin_day).days+1):
+        d_str = day.isoformat()
+        logger.info('start statistic: %s' % d_str)
+        res = pool.apply_async(daily_page_parse.get_daily_reponum, args=(d_str,))
+        result_list.append([d_str, res])
+        day += day.resolution
+    pool.close()
+    pool.join()
+    for pair in result_list:
+        pair[1] = pair[1].get()
+    return result_list
+
+
+if __name__ == '__main__':
+    year = input('year:')
+    year = int(year)
+    begin = date(year, 1, 1)
+    end = date(year, 12, 31)
+    for month in range(1, 12):
+        begin = date(year, month, 1)
+        end = date(year, month, 31)
+        result = stat_by_month(begin, end)
+        result_write.write_csv('%d.csv' % year, result_type=result_write.STATISTIC
+                               , header=['date', 'count'], body_list=result)
