23440b68cda80bf1e63b43b73a1b6fa9c035effe
xzk-seu <xzk09014134@outlook.com>
2018-09-30 17:44:10 +0800

    增加土豪代理

--- get_response.py
+++ get_response.py
@@ -9,12 +9,27 @@ _AGENTS = ip_pool._AGENTS
 ip = IPProvider()
 _HEADERS = {'User-Agent': random.choice(_AGENTS)}
 
+proxyHost = "http-dyn.abuyun.com"
+proxyPort = "9020"
+proxyUser = "H5JJ8231M78WW99D"
+proxyPass = "C9E7BCAEFC8D2098"
+proxyMeta = "http://%(user)s:%(pass)s@%(host)s:%(port)s" % {
+    "host": proxyHost,
+    "port": proxyPort,
+    "user": proxyUser,
+    "pass": proxyPass,
+}
+_PROXIES = {
+    "http": proxyMeta,
+    "https": proxyMeta,
+}
+
 
 def get_response(url, param=None):
     pr = ip.get_ip()
     # logger = my_logger.get_logger(log_file_name='response.log')
     # logger.info(str(pr))
-    r = requests.get(url=url, params=param, headers=_HEADERS, proxies=pr)
+    r = requests.get(url=url, params=param, headers=_HEADERS, proxies=_PROXIES)
     r.raise_for_status()
     r.encoding = r.apparent_encoding
     return r
